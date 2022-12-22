a7545229757d8093e81a7c47c146ed57abc0e5e8
user1 <dastan.namazbekov@mail.ru>
2022-12-22 20:40:40 +0600

    2.2. Модель данных - Post. [#285211]

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -5,11 +5,66 @@ import java.util.Objects;
 
 public class Post {
 
-    int id;
-    String tittle;
-    String link;
-    String description;
-    LocalDateTime create;
+    private int id;
+    private String tittle;
+    private String link;
+    private String description;
+    private LocalDateTime create;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTittle() {
+        return tittle;
+    }
+
+    public void setTittle(String tittle) {
+        this.tittle = tittle;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreate() {
+        return create;
+    }
+
+    public void setCreate(LocalDateTime create) {
+        this.create = create;
+    }
+
+    public Post(int id, String tittle, String link, String description, LocalDateTime create) {
+        this.id = id;
+        this.tittle = tittle;
+        this.link = link;
+        this.description = description;
+        this.create = create;
+    }
+
+    public Post(String tittle, String link, String description, LocalDateTime create) {
+        this.tittle = tittle;
+        this.link = link;
+        this.description = description;
+        this.create = create;
+    }
 
     @Override
     public boolean equals(Object o) {
