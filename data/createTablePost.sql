fdd2f6e8b8e3280545d5480330c2ff323c4273d4
user1 <dastan.namazbekov@mail.ru>
2023-05-03 19:32:59 +0600

    4. Схема таблицы Post [#1731]

--- data/createTablePost.sql
+++ data/createTablePost.sql
@@ -1,7 +1,6 @@
 create table if not exists Post (
     id serial primary key,
-    id  int,
-    name varchar(255),
+    name varchar(255) unique,
     text varchar(255),
     link varchar(255),
     created timestamp
